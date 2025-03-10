8d8761dcfa0c3ebaebe8a589a819bcb1af7190cd
Julian Cortes <mmm808383@gmail.com>
2025-03-09 19:09:10 -0500

    Agregamos la carpeta img/icons agregamos el icon de Sun, realizamos cambios en el header

--- src/assets/components/Layouts/Header/Header.jsx
+++ src/assets/components/Layouts/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../../Styles/header.css'
 import logo from '../../../react.svg'
+import Sun from '../../../../img/icons/IconSun.png'
 
 export const Header = () => {
   return (
@@ -12,18 +13,25 @@ export const Header = () => {
         <div className='logo'>
 
             <img src={logo} alt="logo" className='logo-img' />
-            <h1>My App</h1>
+            <h1>Portfolio</h1>
 
         </div>
 
         <nav>
             <ul>
-                <li><a href="#">Hoome</a></li>
+                <li><a href="#">Home</a></li>
                 <li><a href="#">About Us</a></li>
-                <li><a href="#">History</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Contact</a></li>
+
 
             </ul>
         </nav>
+
+        <div className="bg-container">
+            <img src={Sun} alt="logo" className='logo-sun' />
+        </div>
 
 
     </header>
