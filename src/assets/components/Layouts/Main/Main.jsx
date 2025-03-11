482f6ad8fabd7a0e1818697589e81b19da9fdd0e
Julian-codee <mmm808383@gmail.com>
2025-03-11 10:02:37 -0500

    Realizamos cambios en el main Princpial de la pagina añadimos informacion y editamos su css

--- src/assets/components/Layouts/Main/Main.jsx
+++ src/assets/components/Layouts/Main/Main.jsx
@@ -9,10 +9,15 @@ export const Main = () => {
     
     <main className='mainContent'>
 
-        <h2>I´m Jul!an Dev</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos consequatur
-             unde eos harum a repellendus? Officiis, nam quae doloribus, dicta inventore 
-             tempore, praesentium porro aspernatur cumque nobis laborum accusantium atque.</p>
+        <h2 className='title'>Hello, I´m 
+          <span> Jul!an Dev</span>
+        </h2>
+        <p className='subTitle'>FullStack Developer & UI/UX Designer</p>
+
+        <div className="button-log">
+         <a href="#" className='btn-Primary'>View My Work</a>
+         <a href="#" className='btn-secondary'>Learn More</a>
+        </div>
 
     </main>
     </>
