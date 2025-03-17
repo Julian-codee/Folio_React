5f25a78a14b2c18668f779bf30d0bfd4d1a778ca
Julian Cortes <mmm808383@gmail.com>
2025-03-17 15:31:05 -0500

    Creamos el scroll, para nuestro proyecto

--- src/assets/components/Layouts/Main/Main.jsx
+++ src/assets/components/Layouts/Main/Main.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'//Importamos useRef para hacer scroll
 import '../../../Styles/main.css'
 import logoPersonal from '../../../../img/ME2.png'
 import arrow from '../../../../img/icons/ArrowRight.png'
@@ -9,39 +10,58 @@ import { Footer } from '../Footer/Footer'
 
 
 export const Main = () => {
+
+  /***
+   *Crearemos una funcion para hacer scroll hacia un componente */
+
+  // Creamos una referencia para el componente MainAboutMe
+  const aboutMeRef = useRef(null)
+
+  // Función para hacer scroll hasta el componente
+  const scrollToAboutMe = () => {
+    aboutMeRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
   return (
     <>
-    
-    <main className='mainContent'>
-
-        <video src={Video} width='450px' autoPlay loop muted className='logoPersonal-Video'>
+      <main className="mainContent">
+        <video
+          src={Video}
+          width="450px"
+          autoPlay
+          loop
+          muted
+          className="logoPersonal-Video"
+        >
           {/*Tipos de video soportados para todos los navegadores*/}
-          <source src={Video} type="video/mp4"/>
-          <source src={Video} type="video/webm"/>
-          <source src={Video} type="video/ogg"/>
-          <source src={Video} type="video/mov"/>
-
+          <source src={Video} type="video/mp4" />
+          <source src={Video} type="video/webm" />
+          <source src={Video} type="video/ogg" />
+          <source src={Video} type="video/mov" />
           tu navegador no soporta el video
-
         </video>
 
-        <h2 className='title'>Hello, I´m 
+        <h2 className="title">
+          Hello, I´m
           <span> Jul!an Dev</span>
         </h2>
-        <p className='SubTitle'>FullStack Developer & UI/UX Designer</p>
+        <p className="SubTitle">FullStack Developer & UI/UX Designer</p>
 
         <div className="button-log">
-         <a href="#" className='btn-Primary'>View My Work <img src={arrow} alt="Arrow" className='ArrowRight' /></a>
-         <a href="#" className='btn-secondary'>Learn More <img src={arrowDown} alt="Arrow" className='ArrowDown' /></a>
+          <a href="#" className="btn-Primary">
+            View My Work <img src={arrow} alt="Arrow" className="ArrowRight" />
+          </a>
+          <a onClick={scrollToAboutMe} className="btn-secondary">
+            Learn More <img src={arrowDown} alt="Arrow" className="ArrowDown" />
+          </a>
         </div>
+      </main>
 
-      
+      {/*Referencia para el desplazamiento*/}
+      <div ref={aboutMeRef}>
+        <MainAboutMe />
+      </div>
 
-    </main>
-
-    <MainAboutMe />
-
-    <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
