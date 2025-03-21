354ad796e8160512028838f12e55a44d61411270
Julian Cortes <mmm808383@gmail.com>
2025-03-20 22:14:39 -0500

    Creamos la estructura de MyProjects y Cards

--- src/assets/components/Layouts/Main/Myprojects.jsx
+++ src/assets/components/Layouts/Main/Myprojects.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import '../../../Styles/Myprojects.css'
+import { Cards } from '../../Ui/Cards'
 
 export const Myprojects = () => {
   return (
-    <div>
 
-        <h1>My Projects</h1>
-      
-    </div>
+    <>
+    <div className="main-project">
+        <div className="ContainerProject">
+        <h2 className="Project-title">My Projects</h2>
+        <hr className="divider" />
+        <p className="description">
+        Explore my recent work and projects that showcase my skills and expertise.
+        </p>
+      </div>
+
+      <Cards />
+
+      </div>
+
+    </>
   )
 }
+
 
---
+++ src/assets/components/Ui/Cards.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../../Styles/Cards.css'
+
+export const Cards = () => {
+  return (
+  <>
+
+<div class="container">
+        <div class="card">
+            <div class="image image-dark">
+                <button class="btn">Demo</button>
+                <button class="btn">Code</button>
+            </div>
+            <div class="content">
+                <h3>E-commerce Website</h3>
+                <p> A modern e-commerce platform with a sleek UI and seamless checkout experience.</p>
+                <div class="tags">
+                    <span>React</span>
+                    <span>Next.js</span>
+                    <span>Tailwind CSS</span>
+                    <span>Stripe</span>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  
+  </>
+  )
+}
+
+
