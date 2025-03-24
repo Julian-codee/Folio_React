e64215ae7b8c714e4f8a34e02afb2a6a5b26659a
Julian Cortes <mmm808383@gmail.com>
2025-03-24 09:45:21 -0500

    Creamos el componente TechnicalSkils

--- src/assets/components/Layouts/Main/Skills.jsx
+++ src/assets/components/Layouts/Main/Skills.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { TechnicalSkills } from '../../Ui/TechnicalSkills'
 
 export const Skills = () => {
   return (
     <div>
 
         <h1>hola soy Skills</h1>
+
+        <TechnicalSkills />
       
     </div>
   )
---
+++ src/assets/components/Ui/TechnicalSkills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const TechnicalSkills = () => {
+  return (
+   <>
+
+    <h4>hola</h4>
+
+   </>
+  )
+}
+
+
