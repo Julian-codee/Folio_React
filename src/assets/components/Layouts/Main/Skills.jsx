1a073c5bc85d3f70ca52ffa866356f13a2baf9b0
Julian Cortes <mmm808383@gmail.com>
2025-03-24 19:33:15 -0500

    Realizamos estilos y maquetacion de skilss

--- src/assets/components/Layouts/Main/Skills.jsx
+++ src/assets/components/Layouts/Main/Skills.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 import { TechnicalSkills } from '../../Ui/TechnicalSkills'
 
 export const Skills = () => {
+
   return (
-    <div>
+    <>
+    <div className="main-Skills">
+        <div className="ContainerSkills">
+          <h2 className="Project-title">My Projects</h2>
+          <hr className="divider" />
+          <p className="description">
+            Explore my recent work and projects that showcase my skills and expertise.
+          </p>
+        </div>
+        </div>
+        
 
-        <h1>hola soy Skills</h1>
 
-        <TechnicalSkills />
-      
-    </div>
-  )
-}
+
+    </>
+  );
+};
 
 
--- src/assets/components/Ui/TechnicalSkills.jsx
+++ src/assets/components/Ui/TechnicalSkills.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import '../../Styles/TechnicalSkilss.css';
+import { h1 } from 'framer-motion/client';
 
 export const TechnicalSkills = () => {
   return (
-   <>
 
-    <h4>hola</h4>
+    <>
+    
 
-   </>
-  )
-}
+    </>
 
-
+  );
+};
