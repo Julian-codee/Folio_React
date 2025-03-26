80d83420ce185a6377c40486661b3287801e22d6
Julian Cortes <mmm808383@gmail.com>
2025-03-25 22:23:39 -0500

    hacemos los ultimos arreglos en el diseño de las skills tecnicas del proyecto

--- src/assets/components/Layouts/Main/Skills.jsx
+++ src/assets/components/Layouts/Main/Skills.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import { TechnicalSkills } from '../../Ui/TechnicalSkills'
+import { Footer } from '../Footer/Footer'
+import '../../../Styles/Skilss.css'
 
 export const Skills = () => {
 
   return (
     <>
-    <div className="main-Skills">
+      <div className="main-Skills">
         <div className="ContainerSkilss">
-          <h2 className="Project-title">My Skils</h2>
+          <h2 className="Project-title">My Projects</h2>
           <hr className="divider" />
           <p className="description">
             Explore my recent work and projects that showcase my skills and expertise.
           </p>
         </div>
 
-        
-        </div>
+        <div className="Skilss">
+
         <TechnicalSkills />
+       
+
+        </div>
+
+      </div>
+
+      <Footer />
+
+
 
 
 
--- src/assets/components/Ui/TechnicalSkills.jsx
+++ src/assets/components/Ui/TechnicalSkills.jsx
@@ -7,13 +7,52 @@ export const TechnicalSkills = () => {
 
     <>
     <div className="progressSkill">
+      <h3>Technical Skilss</h3>
       <div className="infoProgress">
-      <h4>HTML</h4>
+      <h4>HTML & CSS</h4>
       <span className="progressLabel">95%</span>
       </div>
       <progress value={95} max={100}></progress>
+
+      <div className="infoProgress">
+      <h4>JavaScript</h4>
+      <span className="progressLabel">90%</span>
+      </div>
+      <progress value={90} max={100}></progress>
+
+      <div className="infoProgress">
+      <h4>React</h4>
+      <span className="progressLabel">85%</span>
+      </div>
+      <progress value={85} max={100}></progress>
+
+      <div className="infoProgress">
+      <h4>UI / UX Desing</h4>
+      <span className="progressLabel">90%</span>
+      </div>
+      <progress value={90} max={100}></progress>
+
+      <div className="infoProgress">
+      <h4>Node JS</h4>
+      <span className="progressLabel">70%</span>
+      </div>
+      <progress value={70} max={100}></progress>
+
+      <div className="infoProgress">
+      <h4>Java</h4>
+      <span className="progressLabel">65%</span>
+      </div>
+      <progress value={65} max={100}></progress>
+
+      <div className="infoProgress">
+      <h4>MySql</h4>
+      <span className="progressLabel">75%</span>
+      </div>
+      <progress value={75} max={100}></progress>
    
     </div>
+
+    
 
 
     </>
