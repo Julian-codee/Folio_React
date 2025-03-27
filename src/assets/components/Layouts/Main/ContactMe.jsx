8ed6a2df53ea8e74106715ae7d0c98c53371cb86
Julian Cortes <mmm808383@gmail.com>
2025-03-27 18:03:37 -0500

    creamos el archivo de los estilos de contactme , para empezar con su estrucutra y diseño

--- src/assets/components/Layouts/Main/ContactMe.jsx
+++ src/assets/components/Layouts/Main/ContactMe.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import '../../../Styles/ContactMe.css'
 
 export const ContactMe = () => {
   return (
-    <div>
-        <h1>Hola soy contacto</h1>
-      
-    </div>
+<>
+
+<div className="main-Contact">
+        <div className="ContainerContact">
+          <h2 className="Contact-title">Get in Touch</h2>
+          <hr className="divider" />
+          <p className="description">
+          Have a project in mind or want to collaborate? Feel free to reach out to me.
+          </p>
+        </div>
+        </div>
+
+</>
   )
 }
 
